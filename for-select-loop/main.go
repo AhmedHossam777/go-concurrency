3ca75f99d5c56026db1ea1094bf978a3ffd9bbd2
AhmedHossam777 <ahmedhossam9226@gmail.com>
2025-12-17 23:35:12 +0200

    for select loop

--- for-select-loop/main.go
+++ for-select-loop/main.go
@@ -1,1 +1,28 @@
 package main
+
+import "fmt"
+
+func main() {
+	charChannel := make(chan string, 3)
+	chars := [3]string{"a", "b", "c"}
+
+	for _, char := range chars {
+		select {
+		case charChannel <- char:
+
+		}
+	}
+
+	close(charChannel)
+
+	//* the following lines will return only the first character as the channel get out the data like queue
+	//result := <-charChannel
+	//fmt.Println(result)
+
+	//* to get the all data we will use a for loop
+	for result := range charChannel {
+		fmt.Println(result) // a  b  c
+	}
+
+	fmt.Println("hello,this is \"for select\" concurrency pattern")
+}
