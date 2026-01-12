9b1ed6024a858ee8950124480984d1c3615ca0ba
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-01-12 19:32:59 +0200

    select with concurreny

---
+++ select/go.mod
@@ -0,0 +1,3 @@
+module github/AhmedHossam777/select
+
+go 1.25.4
---
+++ select/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	myChannel := make(chan string)
+	anotherChannel := make(chan string)
+	
+	go func() {
+		myChannel <- "data"
+	}()
+	
+	go func() {
+		anotherChannel <- "cow"
+	}()
+	
+	// this select statement will block until one of it's statement runs
+	select {
+	case messageFromMyChannel := <-myChannel:
+		fmt.Println(messageFromMyChannel)
+	case messageFromAnotherChannel := <-anotherChannel:
+		fmt.Println(messageFromAnotherChannel)
+	}
+}
