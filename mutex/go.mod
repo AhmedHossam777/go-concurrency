ff243e139583e8081146f2b606048bdd125edc69
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-02-12 04:47:18 +0200

    simple usage of mutex soliving race conditioning

---
+++ mutex/go.mod
@@ -0,0 +1,3 @@
+module github/AhmedHossam777/mutex
+
+go 1.25.6
---
+++ mutex/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var msg string
+var wg sync.WaitGroup
+
+func updateMessage(s string, mu *sync.Mutex) {
+	defer wg.Done()
+	mu.Lock()
+	msg = s
+	mu.Unlock()
+}
+
+func main() {
+	msg = "hello, world"
+	
+	var mutex sync.Mutex
+	// here is the problem of the race condition
+	// two goroutines is trying to modofy the same data
+	// we realy don't know which goroutine will
+	wg.Add(2)
+	go updateMessage("hello, universe!", &mutex)
+	go updateMessage("hello, cosmos!", &mutex)
+	wg.Wait()
+	
+	fmt.Println(msg)
+}
